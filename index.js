396d0e062d86bc7ecc7ddb6f2cad803238a46905
bhatt396 <079bei013.bhaskar@pcampus.edu.np>
2023-12-07 14:30:09 +0545

    simple-form

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+        function submitForm() {
+            // Get form values
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var email = document.getElementById("email").value;
+
+            // Display values (you can modify this part based on your needs)
+            alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nEmail: " + email);
+        }
+    </script>
